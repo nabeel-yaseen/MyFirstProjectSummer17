4259e365981a09fdad42e21904de4bf229b1a617
Nabeel Yaseen <nabeel.yaseen10@gmail.com>
2017-07-13 04:35:21 +0500

    Second Commit

--- app/src/main/java/com/example/toshiba/myfirstprojectsummer17/MainActivity.java
+++ app/src/main/java/com/example/toshiba/myfirstprojectsummer17/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.toshiba.myfirstprojectsummer17;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MTAG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate: ");
     }
 }
