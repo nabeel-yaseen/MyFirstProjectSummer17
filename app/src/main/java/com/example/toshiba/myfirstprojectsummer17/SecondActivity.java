dcf89aaa7647a77ae4ae5e72bd2f87d51ad6741e
Nabeel Yaseen <nabeel.yaseen10@gmail.com>
2017-07-13 04:45:47 +0500

    4th Commit

--- app/src/main/java/com/example/toshiba/myfirstprojectsummer17/SecondActivity.java
+++ app/src/main/java/com/example/toshiba/myfirstprojectsummer17/SecondActivity.java
@@ -9,5 +9,7 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        //This is the 4th commit
     }
 }
